5e9d1c2292de1771e0b729388e79b75f87057b2e
smuhtasim <56603189+smuhtasim@users.noreply.github.com>
2022-03-15 15:31:01 +0600

    mapped the cells with gameboard using js

--- Grid.js
+++ Grid.js
@@ -7,5 +7,32 @@ export default class Grid {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+    this.cells = createCellElements(gridElement).map((CellElement, index) => {
+      return new Cell(
+        CellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      );
+    });
+    console.log(this.cells);
   }
+}
+
+class Cell {
+  constructor(CellElement, x, y) {
+    this.CellElement = CellElement;
+    this.x = x;
+    this.y = y;
+  }
+}
+
+function createCellElements(gridElement) {
+  const cells = [];
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cells.push(cell);
+    gridElement.append(cell);
+  }
+  return cells;
 }
